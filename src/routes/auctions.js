123ddc9aadb37b175ca49f05b94753bd5bb436af
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-03-24 14:08:10 -0400

    Hi

--- src/routes/auctions.js
+++ src/routes/auctions.js
@@ -9,6 +9,20 @@ module.exports = db => {
       );
     });
   });
-
+  
+  router.put("/auctions/:id", (request, response) => {
+    const {vehicle_id, dealer_id, bid } = request.body.auction;
+    
+    db.query(
+      `INSERT INTO auctions (vehicle_id, dealer_id, bid)
+      VALUES ($1::integer, $2::integer, $3::integer) RETURNING *
+    `,
+    [vehicle_id, dealer_id, bid]
+    )
+      .then((res) => {
+        response.status(204).json({message: "Successfully Inserted"});
+      })
+      .catch(error => setTimeout(() => {console.log(error)}, 20000));
+  });
   return router;
 };
